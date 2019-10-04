47c06f9058ce2fcbf6de44f77c96d815925609e2
edu-05 <edu-05@edu-01-HP>
2019-10-04 10:17:33 +0900

    교통카드 있으면 차 타고 아니면 걸어가야 됌

---
+++ src/hello/HelloClass.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class HelloClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("헬로 문자열");
+	System.out.println("헬로 문자열");
+	System.out.println(1234);
+	System.out.println("숫자:" + 1234);
+	System.out.println(1 + 3);
+	System.out.println("1" + 3);
+	System.out.println("1 + 3 = " +1 +3);
+
+	}
+
+}
